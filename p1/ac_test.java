b06af2a501ec369852bd86ad8280033e29ae9586
mas682 <47037892+mas682@users.noreply.github.com>
2019-08-27 20:13:12 -0400

    Project 1

---
+++ p1/ac_test.java
@@ -0,0 +1,560 @@
+
+
+//@author Matt Stropkey
+//January 16, 2016
+
+import java.util.Scanner;
+import java.io.File; 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+
+
+public class ac_test {
+	
+	public static class TrieDLB<Value>
+	{
+
+	private Node root;	//root of trie
+	private int n;		//number of keys in trie
+	
+	private class Node {
+		private char c;
+		private Object val;
+		private Node left, mid, right;
+	}
+	
+	public TrieDLB(){
+	}
+	
+	public void put(String key, Value val)
+	{
+		if(key == null)
+			throw new IllegalArgumentException("The String argument to put() is null");
+		if(val == null)
+		{
+			throw new IllegalArgumentException("The Value argument is null");
+		}
+		else
+			root = put(root, key, val, 0);
+	}
+	
+	private Node put(Node x, String key, Value val, int d) {
+		char c = key.charAt(d);
+		if(x == null){
+			x = new Node();
+			x.c = c;
+		}
+		//System.out.println(c);
+		if(c < x.c)
+		{
+			x.left = put(x.left, key, val, d);
+			
+		}
+		else if( c > x.c){
+			try{
+			
+			}catch(NullPointerException e)
+			{
+			}
+			
+			x.right = put(x.right, key, val, d);
+			
+		}
+		else if( d < key.length()-1)
+		{
+			x.mid = put(x.mid, key, val, d+1);
+			
+		}	
+		else
+		{
+			if(d == key.length() - 1){
+				if(x.val == null){
+					n++;
+				} 
+			}
+			x.val = val;
+		}
+		return x;
+		
+	}
+	
+	public Value get(String key)
+	{
+		Node x = get(root, key, 0);
+		if(x == null)
+			return null;
+		return (Value)x.val;
+	}
+	
+	private Node get(Node x, String key, int d)
+	{
+		if(x == null)
+			return null;
+		char c = key.charAt(d);
+		if(c < x.c)
+			return get(x.left, key, d);
+		else if(c > x.c)
+			return get(x.right, key, d);
+		else if(d < key.length()-1)
+			return get(x.mid, key, d+1);
+		else
+			return x;
+	}
+	
+	public boolean contains(String key) {
+		if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+	
+	public int size() {
+        return n;
+    }
+	
+	 /**
+     * Is this symbol table empty?
+     * @return {@code true} if this symbol table is empty and {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+	
+	
+	public LinkedQueue<String> keysWithPrefix(String prefix)
+	{
+		LinkedQueue<String> queue = new LinkedQueue<>();
+		collect(get(root, prefix, 0), prefix, queue);
+		return queue;
+	}
+	private void collect(Node x, String prefix, LinkedQueue<String> queue)
+	{
+		if(x == null) return;
+		if(x.val != null)
+		{			queue.enqueue(prefix);
+					//System.out.println("enqueue: " + prefix);
+		}
+		
+		Node currentNode = x;
+		if(x.mid!= null)
+		{
+			currentNode = x.mid;
+			collect(x.mid, prefix+x.mid.c, queue);
+			
+			if(currentNode.right != null)
+			{
+				currentNode = currentNode.right;
+				collect(currentNode, prefix+currentNode.c, queue);
+				while(currentNode.right != null)
+				{
+					currentNode = currentNode.right;
+					collect(currentNode, prefix+currentNode.c, queue);
+				}
+				return;
+			}
+			
+			if(currentNode.left != null)
+			{
+				currentNode = currentNode.left;
+				collect(currentNode, prefix+currentNode.c, queue);
+				while(currentNode.left != null)
+				{
+					currentNode = currentNode.left;
+					collect(currentNode, prefix+currentNode.c, queue);
+				}
+				return;
+			}
+		}			
+		
+	}
+
+}
+	private static class LinkedQueue<T> {
+    // References node at front of queue
+    private Node firstNode;
+    // References node at back of queue
+    private Node lastNode;
+
+    public LinkedQueue() {
+        firstNode = null;
+        lastNode = null;
+    }
+
+    public void enqueue(T newEntry) {
+        Node newNode = new Node(newEntry, null);
+
+        if (isEmpty()) {
+            firstNode = newNode;
+        } else {
+            lastNode.setNextNode(newNode);
+        }
+
+        lastNode = newNode;
+    }
+	
+	public T dequeue() {
+        // Might throw EmptyQueueException
+        T front = getFront();
+
+        assert firstNode != null;
+        firstNode.setData(null);
+        firstNode = firstNode.getNextNode();
+
+        if (firstNode == null) {
+            lastNode = null;
+        }
+
+        return front;
+    }
+	public T getFront() {
+       // if (isEmpty()) {
+            //throw new EmptyQueueException();
+        //} else {
+            return firstNode.getData();
+        //}
+    }
+
+    public boolean isEmpty() {
+        return (firstNode == null) && (lastNode == null);
+    }
+	 private class Node {
+        // Entry in queue
+        private T data;
+        // Link to next node
+        private Node next;
+
+        private Node(T dataPortion) {
+            data = dataPortion;
+            next = null;
+        }
+
+        private Node(T dataPortion, Node linkPortion) {
+            data = dataPortion;
+            next = linkPortion;
+        }
+
+        private T getData() {
+            return data;
+        }
+
+        private void setData(T newData) {
+            data = newData;
+        }
+
+        private Node getNextNode() {
+            return next;
+        }
+
+        private void setNextNode(Node nextNode) {
+            next = nextNode;
+        }
+    }
+}
+public static void main(String[] args) {
+
+        // build symbol table from standard input
+        TrieDLB<Integer> st = new TrieDLB<Integer>();
+		TrieDLB<Integer> userIn = new TrieDLB<Integer>();
+		try{
+		readFile("dictionary.txt", st);
+		}catch (FileNotFoundException e) { 
+		System.err.println("File dictionary.txt not found");
+		}
+		try{
+			readFile("user_history.txt", userIn);
+		}
+		catch(FileNotFoundException e)
+		{
+			System.err.println("File user_history not found");
+		}
+		
+		String input = null;
+		String prefix = "";
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String [] list = new String[5];
+		long startTime = 0;
+		long elapsedTime = 0;
+		double seconds = 0;
+		boolean repeat = true;
+		boolean terminate = true;
+		String[] suggestions = new String[5];
+		double avgTime = 0;
+		int counter = 0;
+		do{
+			repeat = true;
+			prefix = "";
+		do{
+			startTime = 0;
+			elapsedTime = 0;
+			seconds = 0;
+			int i = 1;
+			int a = 0;
+			String word = null;
+			if(prefix.length() == 0)
+			{
+				boolean again = false;
+				do{
+					System.out.print("Enter your first character: ");
+					again = false;
+					if(scan.hasNextInt())
+					{
+						again = true;
+					}
+					input = scan.next();
+					if(input.charAt(0) == '!')
+					{
+						terminate = false;
+						repeat = false;
+					}
+					if(input.charAt(0) == '$')
+					{
+						again = true;
+					}
+				}while(input.length() > 1 || again);
+				prefix = input;
+				if(terminate)
+				{
+				startTime = System.nanoTime();
+				list = getSuggestions(input, st, userIn);
+				elapsedTime = System.nanoTime() - startTime;
+				seconds = (double)elapsedTime/1000000000.0;
+				avgTime += seconds;
+				counter++;
+				System.out.printf("\n(%.6f s)\n", seconds);
+				System.out.println("Predictions:");
+				suggestions = new String[5];
+				while(a < 5 && list[a] != null)
+				{
+					word = list[a];
+					System.out.print("(" + i + ")  " + word + "		");
+					suggestions[a] = word;
+					a++;
+					i++;
+				}
+				if(list[0] == null)
+				{
+					System.out.println("No suggestions available.");
+				}
+				System.out.println();
+				}
+			}
+			else
+			{
+				boolean again;
+				do{
+					again = false;
+					System.out.print("Enter the next character: ");
+					if(scan.hasNextInt())
+					{
+						input = scan.next();
+						if(input.charAt(0) == '1' && suggestions[0] != null)
+						{
+							System.out.println("\n\nWORD COMPLETED: " + suggestions[0]);
+							System.out.println();
+							repeat = false;
+							if(!userIn.contains(suggestions[0]))
+							{
+							save(suggestions[0]);
+							userIn.put(suggestions[0], userIn.size()+1);
+							}
+							break;
+						}
+						else if(input.charAt(0) == '2' && suggestions[1] != null)
+						{
+							System.out.println("\n\nWORD COMPLETED: " + suggestions[1]);
+							System.out.println();
+							repeat = false;
+							if(!userIn.contains(suggestions[1]))
+							{
+							save(suggestions[1]);
+							userIn.put(suggestions[1], userIn.size()+1);
+							}
+							break;
+						}
+						else if(input.charAt(0) == '3' && suggestions[2] != null)
+						{
+							System.out.println("\n\nWORD COMPLETED: " + suggestions[2]);
+							System.out.println();
+							repeat = false;
+							if(!userIn.contains(suggestions[2]))
+							{
+							save(suggestions[2]);
+							userIn.put(suggestions[2], userIn.size()+1);
+							}
+							break;
+						}
+						else if(input.charAt(0) == '4' && suggestions[2] != null)
+						{
+							System.out.println("\n\nWORD COMPLETED: " + suggestions[3]);
+							System.out.println();
+							repeat = false;
+							if(!userIn.contains(suggestions[3]))
+							{
+							save(suggestions[3]);
+							userIn.put(suggestions[3], userIn.size()+1);
+							}
+							break;
+						}
+						else if(input.charAt(0) == '5' && suggestions[4] != null)
+						{
+							System.out.println("\n\nWORD COMPLETED: " + suggestions[4]);
+							System.out.println();
+							repeat = false;
+							if(!userIn.contains(suggestions[4]))
+							{
+							save(suggestions[4]);
+							userIn.put(suggestions[4], userIn.size()+1);
+							}
+							break;
+						}
+						else
+						{
+							System.out.println("\nInvalid number choice");
+							System.out.println();
+							again = true;
+						}
+					}
+					if(again == false)
+					{
+						input = scan.next();
+						if(input.charAt(0) == '$')
+						{
+							System.out.println("\n\nWORD COMPLETED: " + prefix);
+							System.out.println();
+							save(prefix);
+							userIn.put(prefix, userIn.size()+1);
+							
+							repeat = false;
+						}
+						if(input.charAt(0) == '!')
+						{
+							repeat = false;
+							terminate = false;
+						}
+					}
+
+				}while(input.length() > 1 || again);
+				startTime = 0;
+				elapsedTime = 0;
+				seconds = 0;
+				if(repeat && terminate)
+				{
+				prefix = prefix + input;
+				startTime = System.nanoTime();
+				list = getSuggestions(prefix, st, userIn);
+				elapsedTime = System.nanoTime() - startTime;
+				seconds = (double)elapsedTime/1000000000.0;
+				avgTime += seconds;
+				counter++;
+				System.out.printf("\n(%.6f s)\n", seconds);
+				System.out.println("Predictions:");
+				i = 1;
+				a = 0;
+				suggestions = new String[5];
+				while(a < 5 && list[a] != null)
+				{
+					word = list[a];
+					System.out.print("(" + i + ")  " + word + "		");
+					suggestions[i-1] = word;
+					i++;
+					a++;
+				}
+				System.out.println();
+				if(list[0] == null)
+				{
+					System.out.println("No suggestions available.");
+				}
+				}
+			}
+		}while(input.charAt(0) != '$' && repeat);
+		}while(terminate);
+		avgTime = avgTime / counter;
+		System.out.printf("\n\nAverage time: %.6f s\n", avgTime);
+		System.out.println("Bye!");
+		
+    }
+	
+		public static void save(String userInput) {
+		try{
+			File file = new File("user_history.txt");
+			PrintWriter printString = new PrintWriter(new FileWriter(file, true));
+			printString.println(userInput);
+			printString.close();
+		}
+		catch(IOException e) {
+			//couldn't save
+			System.out.println("Error: Could not save data");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void readFile(String filename, TrieDLB words)
+	throws FileNotFoundException {
+		
+		Scanner scan = new Scanner(new File(filename));
+		int i = 1;
+		while(scan.hasNext())
+		{
+
+			String symbol = scan.next();
+			words.put(symbol, words.size()+1);
+			i++;
+		}
+		//System.out.println("Number of words: " + words.size());
+	}
+	
+	public static String[] getSuggestions(String input, TrieDLB dictionary, TrieDLB user)
+	{
+
+		int i = 0;
+			String[] suggestions = new String[5];
+			LinkedQueue<String> temp = new LinkedQueue<>();
+			if(!user.isEmpty())
+			{
+				temp = user.keysWithPrefix(input);
+			}
+
+			while(!(temp.isEmpty()) && i < 5)
+			{
+			suggestions[i] = temp.dequeue();
+			//System.out.println(suggestions[i]);
+			//System.out.println("empty: " + temp.isEmpty());
+			i++;
+			}
+			if(i < 5)
+			{
+				if(!dictionary.isEmpty())
+				{
+				temp = dictionary.keysWithPrefix(input);
+				String word;
+				while(!(temp.isEmpty()) && i < 5)
+				{
+					word = temp.dequeue();
+					for(int a = 0; a < i; a++)
+					{
+						if(suggestions[a] != null && suggestions[a].equals(word))
+						{
+							if(!temp.isEmpty())
+							{
+								word = temp.dequeue();
+
+							}
+							else
+							{
+								word = null;
+							}
+						}
+						suggestions[i] = word;
+					}
+					i++;
+				}
+				}
+			}
+			
+			return suggestions;
+	}
+			
+	 
+}
